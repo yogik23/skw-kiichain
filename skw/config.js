a1ee0add3cad278c07ad9264b044228dbb30d534
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-06-30 22:01:06 +0700

    Update config.js

--- skw/config.js
+++ skw/config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 const RPC = "https://json-rpc.uno.sentry.testnet.v3.kiivalidator.com/";
 export const provider = new ethers.JsonRpcProvider(RPC);
 
-export const privateKeys = fs.readFileSync(path.join(__dirname, "privatekey.txt"), "utf-8")
+export const privateKeys = fs.readFileSync(path.join(__dirname, "../privatekey.txt"), "utf-8")
   .split("\n")
   .map(k => k.trim())
   .filter(k => k.length > 0);
