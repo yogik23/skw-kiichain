954c22aeb2bc8ee8565b8fce5c6a1b4f4dd1999a
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-06-30 22:26:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ export const amountUnwarp = RandomAmount(1, 2, 1);
 export const amountswapKIItoUSDT = RandomAmount(0.1, 1, 2);
 export const amountswapKIItoUSDC = RandomAmount(0.1, 1, 2);
 export const amountswapKIItoWBTC = RandomAmount(0.1, 1, 2);
+export const amountaddLP = RandomAmount(10, 15, 0);
 
 async function main() {
   console.clear();
