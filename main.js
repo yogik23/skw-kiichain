7a2926f37d16d1cfd07139512d3172c328eacfee
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-06-30 21:40:32 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+import { ethers } from "ethers";
+import { logger } from "./skw/logger.js";
+import { swap } from "./src/swap.js";
+import { addliquidity } from "./src/addliquidity.js";
+
+import { 
+  provider,
+  privateKeys,
+  delay,
+} from "./skw/config.js";
+
+async function main() {
+  console.clear();
+  try {
+    for (const pk of privateKeys) {
+      const wallet = new ethers.Wallet(pk, provider);
+      logger.account(`Wallet: ${wallet.address}`);
+
+      await swap(wallet);
+      await addliquidity(wallet);
+      await delay(5000);
+
+    }
+  } catch (err) {
+    logger.fail(`Function main Gagal ${err.message || err}\n`);
+  }
+}
+
+main();
