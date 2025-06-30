06e3df3c41afcc3884c3b0fa1b78ef43a144a474
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-06-30 22:49:52 +0700

    Delete skw/amount.js

--- skw/amount.js
+++
@@ -1,8 +0,0 @@
-import { RandomAmount } from "./config.js";
-
-export const amountWarp = RandomAmount(3, 5, 0);
-export const amountUnwarp = RandomAmount(1, 2, 1);
-export const amountswapKIItoUSDT = RandomAmount(0.1, 1, 2);
-export const amountswapKIItoUSDC = RandomAmount(0.1, 1, 2);
-export const amountswapKIItoWBTC = RandomAmount(0.1, 1, 2);
-export const amountaddLP = RandomAmount(10, 15, 0);
