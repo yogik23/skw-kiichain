87cdfba56a41838e2757af47fa92ddc3c6d3ad97
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-06-30 22:36:02 +0700

    Update main.js

--- main.js
+++ main.js
@@ -9,15 +9,7 @@ import {
   privateKeys,
   delay,
   randomdelay,
-  RandomAmount,
 } from "./skw/config.js";
-
-export const amountWarp = RandomAmount(3, 5, 0);
-export const amountUnwarp = RandomAmount(1, 2, 1);
-export const amountswapKIItoUSDT = RandomAmount(0.1, 1, 2);
-export const amountswapKIItoUSDC = RandomAmount(0.1, 1, 2);
-export const amountswapKIItoWBTC = RandomAmount(0.1, 1, 2);
-export const amountaddLP = RandomAmount(10, 15, 0);
 
 async function main() {
   console.clear();
