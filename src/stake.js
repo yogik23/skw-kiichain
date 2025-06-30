6fea9da9e363db8131c5f09497d1bf5a5d7b672a
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-06-30 23:18:20 +0700

    Create stake.js

---
+++ src/stake.js
@@ -0,0 +1,36 @@
+import { ethers } from "ethers";
+import { logger } from "../skw/logger.js";
+import { stake_address } from "../skw/contract.js";
+import { stake_abi } from "../skw/abis.js";
+import { getRandomValidator } from "../skw/config.js";
+
+export async function stake(wallet) {
+  const contract = new ethers.Contract(stake_address, stake_abi, wallet);
+  const validator = getRandomValidator();
+  const amount = ethers.parseUnits("1", 18);
+
+  logger.start(`Staking 1 KII ke ${validator.moniker}`);
+
+  try {
+    const tx = await contract.delegate(
+      wallet.address,
+      validator.address,
+      amount,
+      {
+        gasLimit: 210000n,
+        maxFeePerGas: BigInt("0x22ecb25c00"),
+        maxPriorityFeePerGas: BigInt("0x8bb2c9700")
+      }
+    );
+
+    logger.send(`Tx dikirim ->> ${explorer}${tx.hash}`);
+    const receipt = await tx.wait();
+    if (receipt.status === 1) {
+      logger.succes(`Stake success\n`);
+    } else {
+      logger.fail(`Stake failed\n`);
+    }
+  } catch (err) {
+    logger.fail(`Transaksi Stake Gagal ${err.message || err}\n`);
+  }
+}
